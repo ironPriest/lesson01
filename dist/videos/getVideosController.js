436c811444c152790d1b7ba521665fb0f05e49cc
ironPriest <iron.priest.mih@gmail.com>
2024-11-05 09:07:44 +0300

    tests added and congured

---
+++ dist/videos/getVideosController.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getVideosController = void 0;
+const db_1 = require("../db/db");
+const getVideosController = (req, res /*OutputVideoType[]*/, /*OutputVideoType[]*/ ) => ;
+exports.getVideosController = getVideosController;
+ > ;
+{
+    const videos = db_1.db.videos; // получаем видео из базы данных
+    res
+        .status(200)
+        .json(videos); // отдаём видео в качестве ответа
+}
+// не забудьте добавить эндпоинт в апп
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: "node",
+  testRegex: "__tests__/.*.e2e.test.ts$",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+};
